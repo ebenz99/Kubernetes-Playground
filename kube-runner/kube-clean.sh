d43648dc1099b1520d01120a49b496335f61e482
Ethan Bensman <ebenz99@gmail.com>
2019-09-15 22:22:37 -0400

    no excess folders

--- kube-runner/kube-clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Remove all pods that are not running.
-
-PODS=$(kubectl get pods --no-headers | grep 'Error\|Completed' | awk '{ print $1 }')
-
-if [[ ! -z $PODS ]]; then
-	kubectl delete pods $PODS
-fi
