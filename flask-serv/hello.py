bc6459ccc7183410d74346483d2f6eb67ae7d4a7
Ethan Bensman <ebenz99@gmail.com>
2019-10-07 11:50:39 -0400

    commit stuff

--- flask-serv/hello.py
+++ flask-serv/hello.py
@@ -7,12 +7,6 @@ l = []
 
 @app.route('/data', methods = ['GET','POST'])
 def hello():
-	if request.method == "POST":
-		#print(request.args)
-		name = request.args.get("name")
-		print("HEY THERE %s" % name)
-		l.append(name)
-	print(l)
 	return "Hello World!"
 
 if __name__ == "__main__":
