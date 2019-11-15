67717fe117fe2fd35f01883b6ce3cbd73af4836c
Ethan Bensman <ebenz99@gmail.com>
2019-11-15 10:40:37 -0500

    works on GKE

--- redis/interact.py
+++ redis/interact.py
@@ -1,6 +1,9 @@
 import redis
-#r = redis.Redis(host='localhost', port=6379, db=0)
-#r = redis.StrictRedis(host="192.168.99.103", port=31334, db=0, password=')
-r = redis.StrictRedis(host="192.168.99.103", port=31334, db=0)
+#r = redis.Redis(host='35.196.124.142', port=6379, db=0)
+
+r = redis.Redis(host='34.73.169.5', port=6379, db=0, password='helloworld')
+#r = redis.StrictRedis(host='34.73.169.5', port=6379, db=0, password='helloworld')
+#r = redis.StrictRedis(host='10.108.34.156', port=30012, db=0, password='helloworld')
+#r = redis.StrictRedis(host='35.227.109.179', port=6379, db=0, password='helloworld')
 r.set("key", "value")
 print(r.get('key'))
