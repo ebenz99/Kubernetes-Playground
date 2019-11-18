b6a5f7574af8a8494cef9dd8f4a4d6326179e310
Ethan Bensman <ebenz99@gmail.com>
2019-11-18 13:10:37 -0500

    prp edition - nofunctional

--- redis/interact.py
+++ redis/interact.py
@@ -1,7 +1,8 @@
 import redis
-#r = redis.Redis(host='35.196.124.142', port=6379, db=0)
+#r = redis.Redis(host='scidas-redis.nautilus.optiputer.net', port=6380, db=0, password='helloworld')
+r = redis.Redis(host='https://scidas-redis.nautilus.optiputer.net/redis-master', port=6379, db=0)
 
-r = redis.Redis(host='34.73.169.5', port=6379, db=0, password='helloworld')
+#r = redis.Redis(host='34.73.169.5', port=6379, db=0, password='helloworld')
 #r = redis.StrictRedis(host='34.73.169.5', port=6379, db=0, password='helloworld')
 #r = redis.StrictRedis(host='10.108.34.156', port=30012, db=0, password='helloworld')
 #r = redis.StrictRedis(host='35.227.109.179', port=6379, db=0, password='helloworld')
